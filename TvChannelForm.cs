4747fcf229a7cdea0d5c9d7dc6425ea5bf62d5b9
[Insomnia] <unlucky_v@mail.ru>
2015-03-13 14:55:36 +0300

    Исходник для формы телеканалов

--- TvChannelForm.cs
+++ TvChannelForm.cs
@@ -31,5 +31,33 @@ namespace TV_Advertisement
             this.tV_ChannelTableAdapter.Fill(this.dB_AdvertisementDataSet1.TV_Channel);
 
         }
+
+        private void tV_ChannelDataGridView_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            if (e.Exception != null)
+            {
+                MessageBox.Show("Неверный тип введенных данных или пустое поле!");
+            }
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                for (int i = 0; i < tV_ChannelDataGridView.Rows.Count - 1; i++)
+                {
+                    if (tV_ChannelDataGridView[1, i].Value.ToString().TrimEnd(' ') == textBox1.Text)
+                    {
+                        MessageBox.Show("Телеканал: " + textBox1.Text + " найден!");
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
     }
 }
