d1aa836fe6fc8262dfa571940d5d0b573b617fe4
[Insomnia] <unlucky_v@mail.ru>
2015-03-15 20:28:10 +0300

    хранимая процедура

---
+++ bin/Debug/dbo.SimpleStoreProc.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[SimpleStoredProc]
+(
+	@name nchar(50),
+	@dura int
+)
+AS
+	insert into dbo.Adv_Segment(Adv_Segment.Name,Adv_Segment.Duration,Adv_Segment.OneDay_Count,Adv_Segment.Days_Count) 
+	values (@name,@dura,5,12)
+RETURN 
