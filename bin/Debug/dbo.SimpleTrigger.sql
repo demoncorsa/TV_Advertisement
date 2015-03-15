f45d34eb41778cd4b4bf93d490b415862ad93a37
[Insomnia] <unlucky_v@mail.ru>
2015-03-15 20:28:21 +0300

    Триггер на вставку

---
+++ bin/Debug/dbo.SimpleTrigger.sql
@@ -0,0 +1,8 @@
+﻿CREATE TRIGGER [SimpleTrigger]
+	ON [dbo].[TV_Channel]
+	FOR insert
+	AS
+	BEGIN
+		insert into dbo.TV_Channel(TV_Channel.Id,TV_Channel.Name,TV_Channel.Second_Cost,TV_Channel.Viewers_MaxValue_Month) 
+		values (500,'CNN', 30, 5830)
+	END
